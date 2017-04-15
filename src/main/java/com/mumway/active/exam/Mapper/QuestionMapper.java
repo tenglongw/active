92926dd574e52e67dcc341b9bf15d9f5c8013104
wtl <wtl@AFOED-606271423>
2017-04-15 21:37:56 +0800

    add

---
+++ src/main/java/com/mumway/active/exam/Mapper/QuestionMapper.java
@@ -0,0 +1,18 @@
+package com.mumway.active.exam.Mapper;
+
+import com.mumway.active.exam.domain.Question;
+
+public interface QuestionMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Question record);
+
+    int insertSelective(Question record);
+
+    Question selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Question record);
+
+    int updateByPrimaryKey(Question record);
+    
+}
